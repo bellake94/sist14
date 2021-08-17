7c17ba922a1700c9fbe4bdfe2f209f112f091091
Lee <Lee@DESKTOP-G663GEV>
2021-08-17 21:55:16 +0900

    2. hi 추가 코딩

--- javaPro/src/javaPro/Hellow.java
+++ javaPro/src/javaPro/Hellow.java
@@ -5,6 +5,9 @@ public class Hellow {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hellow world~");
+		
+		// 새로운 팀원 A 추가 작업
+		System.out.println("Hi~");
 	}
 
 }
